8f248bbecc6ac8af03272be887faa7da4b58040a
Bharath <bharath628@gmail.com>
2020-01-22 08:09:26 -0500

    first push

---
+++ src/main/java/com/bmXpress/fist/FistApplication.java
@@ -0,0 +1,15 @@
+package com.bmXpress.fist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FistApplication.class, args);
+
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/bmXpress/fist/FistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmXpress.fist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
